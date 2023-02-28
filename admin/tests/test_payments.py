efceeb777fbc06c61b9a76b46e2c87ea5e6c8a1f
Lautaro Torchia <torchia.lautaro2@gmail.com>
2023-02-28 12:23:43 -0300

    refactor payment test

--- admin/tests/test_payments.py
+++ admin/tests/test_payments.py
@@ -1,6 +1,6 @@
-import requests
 import pytest
 from src.web import create_app
+
 
 @pytest.fixture()
 def app():
@@ -12,30 +12,34 @@ def app():
     )
     yield app
 
+
 @pytest.fixture()
 def client(app):
     return app.test_client()
 
-def test_member_paymets_api():
-    response = requests.get("http://localhost:5000/api/me/payments/1")
+
+def test_member_paymets_api(client):
+    response = client.get("http://localhost:5000/api/me/payments/1")
     assert response.status_code == 200
-    data = response.json()["data"]
+    data = response.get_json()["data"]
     assert len(data) == 5
     assert data["name"] == "Juan"
     assert data["surname"] == "Perez"
     assert len(data["payments"]) == 1
 
-def test_post_payments_api(app):
-    response = requests.get("http://localhost:5000/api/me/payments/1")
+
+def test_post_payments_api(app, client):
+    response = client.get("http://localhost:5000/api/me/payments/1")
     assert response.status_code == 200
-    data = response.json()["data"]
+    data = response.get_json()["data"]
     assert len(data["payments"]) == 1
-    requests.post("http://localhost:5000/api/me/payments/1")
-    response = requests.get("http://localhost:5000/api/me/payments/1")
+    client.post("http://localhost:5000/api/me/payments/1")
+
+    response = client.get("http://localhost:5000/api/me/payments/1")
     assert response.status_code == 200
-    data = response.json()["data"]
+    data = response.get_json()["data"]
     assert len(data["payments"]) == 2
     with app.app_context():
         from src.core.board import delete_payment
+
         delete_payment(data["payments"][-1]["id"])
-    
