32aa93536988d15a60e891baa481a5c318abe282
Ian Petraccaro Cantero <ianpetraccaro@gmail.com>
2023-02-28 10:36:39 -0300

    [Tests] fixed delete payment after post test

--- admin/tests/test_payments.py
+++ admin/tests/test_payments.py
@@ -1,5 +1,20 @@
 import requests
-#from src.core.board import delete_payment
+import pytest
+from src.web import create_app
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update(
+        {
+            "TESTING": True,
+        }
+    )
+    yield app
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
 
 def test_member_paymets_api():
     response = requests.get("http://localhost:5000/api/me/payments/1")
@@ -11,7 +26,7 @@ def test_member_paymets_api():
     assert data["surname"] == "Perez"
     assert len(data["payments"]) == 1
 
-def test_post_payments_api():
+def test_post_payments_api(app):
     response = requests.get("http://localhost:5000/api/me/payments/1")
     print(response)
     assert response.status_code == 200
@@ -23,5 +38,8 @@ def test_post_payments_api():
     assert response.status_code == 200
     data = response.json()["data"]
     assert len(data["payments"]) == 2
-    #delete_payment(2)
+    print(data["payments"][-1]["id"])
+    with app.app_context():
+        from src.core.board import delete_payment
+        delete_payment(data["payments"][-1]["id"])
     
